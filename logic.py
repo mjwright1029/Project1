ae9d0fe324a954a8c4568e90a2f9199229fa18bf
mjwright1029 <151294605+mjwright1029@users.noreply.github.com>
2023-12-07 19:42:26 -0600

    Changed the GUI

--- logic.py
+++ logic.py
@@ -6,3 +6,18 @@ class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.amount_label.hide()
+        self.deposit_button.hide()
+        self.withdraw_button.hide()
+        self.balance_button.hide()
+        self.balance_display.hide()
+
+        font = QFont()
+        font.setPointSize(30)
+
+        self.name_title.setFont(font)
+        self.name_title.setText('NAME: ')
+
+        self.pin_title.setFont(font)
+        self.pin_title.setText('PIN: ')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from logic import *
 
+
 def main():
     application = QApplication([])
     window = Logic()
